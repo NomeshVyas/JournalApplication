c2998b4e8635f848c40a63fe9abc97f4bbe1c5f8
Nomesh Vyas <nomesh9351962867@gmail.com>
2026-04-05 17:38:57 +0530

    V25 - Mockito

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserDetailServiceImplTests.java
@@ -0,0 +1,38 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDetailServiceImplTests {
+    @InjectMocks
+    private UserDetailServiceImpl userDetailServiceImpl;
+
+    @Mock
+    private UserRepository userRepository;
+
+    /* initMocks is deprecated so we should use @ExtendWith(MockitoExtension.class)
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+    */
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(User.builder().username("Shyam").password("123").roles(new ArrayList<>()).build());
+        UserDetails user = userDetailServiceImpl.loadUserByUsername("Shyam");
+        Assertions.assertNotNull(user);
+    }
+}
