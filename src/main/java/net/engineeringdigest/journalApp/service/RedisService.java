1fc54a3d7a3dbdb5e542958e7075babf07347f67
Nomesh vyas <nomesh9351962867@gmail.com>
2026-07-29 01:14:50 +0530

    V39 - applied redis

---
+++ src/main/java/net/engineeringdigest/journalApp/service/RedisService.java
@@ -0,0 +1,39 @@
+package net.engineeringdigest.journalApp.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class RedisService {
+    private final RedisTemplate<Object, Object> redisTemplate;
+
+    public RedisService(RedisTemplate<Object, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public <T> T get(String key, Class<T> entityClass) {
+        try {
+            Object obj = redisTemplate.opsForValue().get(key);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(obj.toString(), entityClass);
+        } catch (Exception e) {
+            log.error("Exception in redis service (get) ", e);
+            return null;
+        }
+    }
+
+    public void set(String key, Object value, Long ttl) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonValue = mapper.writeValueAsString(value);
+            redisTemplate.opsForValue().set(key, jsonValue, ttl, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("Exception in redis service (set)", e);
+        }
+    }
+}
--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -17,15 +17,23 @@ public class WeatherService {
 
     private final RestTemplate restTemplate;
     private final AppCache appCache;
+    private final RedisService redisService;
 
-    public WeatherService(RestTemplate restTemplate, AppCache appCache){
+    public WeatherService(RestTemplate restTemplate, AppCache appCache, RedisService redisService){
         this.restTemplate = restTemplate;
         this.appCache = appCache;
+        this.redisService = redisService;
     }
 
     public WeatherResponse getWeather(String city) {
+        WeatherResponse weatherResponse = redisService.get("weather_of_" + city, WeatherResponse.class);
+        if (weatherResponse != null)
+            return weatherResponse;
+
         String finalApi = appCache.getWeatherApi().replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, WEATHERSTACK_API_KEY);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
+        if(response.getBody() != null)
+            redisService.set("weather_of_" + city, response.getBody(), 300L);
         return response.getBody();
     }
 }
